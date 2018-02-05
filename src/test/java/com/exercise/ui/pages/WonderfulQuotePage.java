b468fddfbf2bf93a3ae9a7ccb590eb672ec78d74
antomyh <antomyh@github.com>
2018-02-05 17:53:35 +0700

    fixing unused imports

--- src/test/java/com/exercise/ui/pages/WonderfulQuotePage.java
+++ src/test/java/com/exercise/ui/pages/WonderfulQuotePage.java
@@ -1,6 +1,5 @@
 package com.exercise.ui.pages;
 
-import com.sun.org.glassfish.gmbal.DescriptorFields;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
@@ -9,9 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Duration;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -54,79 +51,79 @@ public class WonderfulQuotePage extends PageObject {
     private WebElementFacade linkBlibli;
 
 
-    public void inputTxtAreaQuote(String quote){
+    public void inputTxtAreaQuote(String quote) {
         txtAreaQuote.type(quote);
     }
 
-    public void inputTxtInputAuthor(String authorName){
+    public void inputTxtInputAuthor(String authorName) {
         txtInputAuthor.type(authorName);
     }
 
-    public void selectBackgroundColor(String color){
+    public void selectBackgroundColor(String color) {
         selectBackgroundColor.selectByVisibleText(color);
     }
 
-    public void clickBtnAddQuote(){
+    public void clickBtnAddQuote() {
         btnAddQuote.click();
     }
 
-    public Integer getTotalQuote(){
-        return Integer.parseInt(progressBar.getAttribute("aria-valuenow"))/10;
+    public Integer getTotalQuote() {
+        return Integer.parseInt(progressBar.getAttribute("aria-valuenow")) / 10;
     }
 
-    public void scrollDown(){
+    public void scrollDown() {
         JavascriptExecutor jse = (JavascriptExecutor) getDriver();
         jse.executeScript("window.scrollBy(0,250)");
     }
 
-    public void scrollUp(){
+    public void scrollUp() {
         JavascriptExecutor jse = (JavascriptExecutor) getDriver();
         jse.executeScript("window.scrollBy(0,-250)");
     }
 
-    public void clickTabTableView(){
+    public void clickTabTableView() {
         tabTableView.click();
     }
 
-    public void hoverBtnBlue(){
+    public void hoverBtnBlue() {
         Actions actions = new Actions(getDriver());
         actions.moveToElement(btnBlue).build().perform();
     }
 
-    public void hoverBtnRed(){
+    public void hoverBtnRed() {
         Actions actions = new Actions(getDriver());
         actions.moveToElement(btnRed).build().perform();
     }
 
-    public void clickGreen(){
+    public void clickGreen() {
         btnGreen.click();
     }
 
-    public boolean isTableQuoteDisplayed(){
+    public boolean isTableQuoteDisplayed() {
         try {
             WebElementFacade tableQuote = find(By.xpath(xpathTableQuote));
             tableQuote.setImplicitTimeout(new Duration(15, TimeUnit.SECONDS));
             tableQuote.waitUntilPresent();
             return true;
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
 
-    public void clickLinkSource(){
+    public void clickLinkSource() {
         linkSource.click();
     }
 
-    public void clickLinkBlibli(){
+    public void clickLinkBlibli() {
         linkBlibli.click();
     }
 
-    public void switchTab(Integer index){
+    public void switchTab(Integer index) {
         ArrayList<String> tabs = new ArrayList<String>(getDriver().getWindowHandles());
         getDriver().switchTo().window(tabs.get(index));
     }
 
-    public void closeTab(){
+    public void closeTab() {
         getDriver().close();
     }
 
